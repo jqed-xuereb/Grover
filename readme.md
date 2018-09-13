1e3d3a02ab19aa8e6cc079f2e779d6c168913d91
jqed-xuereb <thejakex1@gmail.com>
2018-09-13 13:07:20 +0200

    Added 3-SAT Grover

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # QISKit Grover's
 This project includes a formulation of Grover's Algorithm using IBM's QISKit package on Python.
 It's my first implimentation of a Quantum Algorithm and should be considered an attempt at an introductory exercise to attain a better understanding.
-This project  was inspired by and done during [QuID 2018](qid.ethz.cz) at ETH Zurich.
+This project  was inspired by and done during [QuID 2018](qid.ethz.cz) at ETH Zurich and owes a lot to [this](https://nbviewer.jupyter.org/github/Qiskit/qiskit-tutorial/blob/master/reference/algorithms/grover_algorithm.ipynb) pre-existing implentation of Grover's Algorithm in QISKit.
--- src/grover.py
+++ src/grover.py
@@ -1,10 +1,21 @@
-from qiskit import register, available_backends, get_backend
-from qiskit import ClassicalRegister, QuantumRegister
-from qiskit import QuantumCircuit, execute
-from qiskit.tools.visualization import plot_histogram, matplotlib_circuit_drawer as drawer, qx_color_scheme
-from qiskit import QuantumProgram
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import available_backends, execute, register, get_backend, compile
+
+from qiskit.tools import visualization
+from qiskit.tools.visualization import circuit_drawer
 import Qconfig
 
+q = QuantumRegister(6)
+qc = QuantumCircuit(q)
+qc.x(q[2])
+qc.cx(q[1], q[5])
+qc.cx(q[2], q[5])
+qc.cx(q[3], q[5])
+qc.ccx(q[1], q[2], q[4])
+qc.ccx(q[3], q[4], q[5])
+qc.ccx(q[1], q[2], q[4])
+qc.x(q[2])
+circuit_drawer(qc)
 qx_config = {
     "APItoken": Qconfig.APItoken,
     "url": Qconfig.config['url']}
@@ -12,35 +23,148 @@ qx_config = {
 #set api
 register(qx_config['APItoken'], qx_config['url'])
 
-# set up registers and program
-qr = QuantumRegister(16)
-cr = ClassicalRegister(16)
-qc = QuantumCircuit(qr, cr)
+def black_box_u_f(circuit, f_in, f_out, aux, n, exactly_1_3_sat_formula):
+    """Circuit that computes the black-box function from f_in to f_out.
 
-# rightmost eight (qu)bits have ')' = 00101001 .x is like identity
-qc.x(qr[0]) 
-qc.x(qr[3])
-qc.x(qr[5])
+    Create a circuit that verifies whether a given exactly-1 3-SAT
+    formula is satisfied by the input. The exactly-1 version
+    requires exactly one literal out of every clause to be satisfied.
+    """
+    num_clauses = len(exactly_1_3_sat_formula)
+    for (k, clause) in enumerate(exactly_1_3_sat_formula):
+        # This loop ensures aux[k] is 1 if an odd number of literals
+        # are true
+        for literal in clause:
+            if literal > 0:
+                circuit.cx(f_in[literal-1], aux[k])
+            else:
+                circuit.x(f_in[-literal-1])
+                circuit.cx(f_in[-literal-1], aux[k])
+        # Flip aux[k] if all literals are true, using auxiliary qubit
+        # (ancilla) aux[num_clauses]
+        circuit.ccx(f_in[0], f_in[1], aux[num_clauses])
+        circuit.ccx(f_in[2], aux[num_clauses], aux[k])
+        # Flip back to reverse state of negative literals and ancilla
+        circuit.ccx(f_in[0], f_in[1], aux[num_clauses])
+        for literal in clause:
+            if literal < 0:
+                circuit.x(f_in[-literal-1])
+    # The formula is satisfied if and only if all auxiliary qubits
+    # except aux[num_clauses] are 1
+    if (num_clauses == 1):
+        circuit.cx(aux[0], f_out[0])
+    elif (num_clauses == 2):
+        circuit.ccx(aux[0], aux[1], f_out[0])
+    elif (num_clauses == 3):
+        circuit.ccx(aux[0], aux[1], aux[num_clauses])
+        circuit.ccx(aux[2], aux[num_clauses], f_out[0])
+        circuit.ccx(aux[0], aux[1], aux[num_clauses])
+    else:
+        raise ValueError('We only allow at most 3 clauses')
+    # Flip back any auxiliary qubits to make sure state is consistent
+    # for future executions of this routine; same loop as above.
+    for (k, clause) in enumerate(exactly_1_3_sat_formula):
+        for literal in clause:
+            if literal > 0:
+                circuit.cx(f_in[literal-1], aux[k])
+            else:
+                circuit.x(f_in[-literal-1])
+                circuit.cx(f_in[-literal-1], aux[k])
+        circuit.ccx(f_in[0], f_in[1], aux[num_clauses])
+        circuit.ccx(f_in[2], aux[num_clauses], aux[k])
+        circuit.ccx(f_in[0], f_in[1], aux[num_clauses])
+        for literal in clause:
+            if literal < 0:
+                circuit.x(f_in[-literal-1])
 
-# second eight (qu)bits have superposition of
-# '8' = 00111000
-# ';' = 00111011
-# these differ only on the rightmost two bits
-qc.h(qr[9]) # Haddamard create superposition on 9
-qc.cx(qr[9],qr[8]) # spread it to 8 with a CNOT
-qc.x(qr[11])
-qc.x(qr[12])
-qc.x(qr[13])
+"""
+Grover search implemented in Qiskit.
 
-# measure
-for j in range(16):
-    qc.measure(qr[j], cr[j])
+This module contains the code necessary to run Grover search on 3
+qubits, both with a simulator and with a real quantum computing
+device. This code is the companion for the paper
+"An introduction to quantum computing, without the physics",
+Giacomo Nannicini, https://arxiv.org/abs/1708.03684. 
+"""
 
-backend = "ibmq_qasm_simulator" 
-shots_sim = 128
+def input_state(circuit, f_in, f_out, n):
+    """(n+1)-qubit input state for Grover search."""
+    for j in range(n):
+        circuit.h(f_in[j])
+    circuit.x(f_out)
+    circuit.h(f_out)
+# -- end function
 
-job_sim = execute(qc, backend, shots=shots_sim)
-stats_sim = job_sim.result().get_counts()
+# Make a quantum program for the n-bit Grover search.
+n = 3
 
-drawer(qc)
-plot_histogram(stats_sim)
+def n_controlled_Z(circuit, controls, target):
+    """Implement a Z gate with multiple controls"""
+    if (len(controls) > 2):
+        raise ValueError('The controlled Z with more than 2 ' +
+                         'controls is not implemented')
+    elif (len(controls) == 1):
+        circuit.h(target)
+        circuit.cx(controls[0], target)
+        circuit.h(target)
+    elif (len(controls) == 2):
+        circuit.h(target)
+        circuit.ccx(controls[0], controls[1], target)
+        circuit.h(target)
+
+
+def inversion_about_average(circuit, f_in, n):
+    """Apply inversion about the average step of Grover's algorithm."""
+    # Hadamards everywhere
+    for j in range(n):
+        circuit.h(f_in[j])
+    # D matrix: flips the sign of the state |000> only
+    for j in range(n):
+        circuit.x(f_in[j])
+    n_controlled_Z(circuit, [f_in[j] for j in range(n-1)], f_in[n-1])
+    for j in range(n):
+        circuit.x(f_in[j])
+    # Hadamards everywhere again
+    for j in range(n):
+        circuit.h(f_in[j])
+
+# Exactly-1 3-SAT formula to be satisfied, in conjunctive
+# normal form. We represent literals with integers, positive or
+# negative, to indicate a Boolean variable or its negation.
+exactly_1_3_sat_formula = [[1, 2, -3], [-1, -2, -3], [-1, 2, 3]]
+
+# Define three quantum registers: 'f_in' is the search space (input
+# to the function f), 'f_out' is bit used for the output of function
+# f, aux are the auxiliary bits used by f to perform its
+# computation.
+f_in = QuantumRegister(n)
+f_out = QuantumRegister(1)
+aux = QuantumRegister(len(exactly_1_3_sat_formula) + 1)
+
+# Define classical register for algorithm result
+ans = ClassicalRegister(n)
+
+# Define quantum circuit with above registers
+grover = QuantumCircuit()
+grover.add(f_in)
+grover.add(f_out)
+grover.add(aux)
+grover.add(ans)
+
+input_state(grover, f_in, f_out, n)
+T = 2
+for t in range(T):
+    # Apply T full iterations
+    black_box_u_f(grover, f_in, f_out, aux, n, exactly_1_3_sat_formula)
+    inversion_about_average(grover, f_in, n)
+
+# Measure the output register in the computational basis
+for j in range(n):
+    grover.measure(f_in[j], ans[j])
+
+# Execute circuit
+job = execute([grover], backend='local_qasm_simulator', shots=1000)
+result = job.result()
+# Get counts and plot histogram
+counts = result.get_counts(grover)
+visualization.plot_histogram(counts)
